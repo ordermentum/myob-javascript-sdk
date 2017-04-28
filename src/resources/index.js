da8918a3b2a046fa1caa1cc598380c1cd10c174a
Michael Cooper <mikeyjc@gmail.com>
2017-04-28 16:24:22 +1000

    add extra resources to sdk

--- src/resources/index.js
+++ src/resources/index.js
@@ -4,6 +4,9 @@ export default {
   inventoryItems: resource('/Inventory/Item', 'Items'),
   invoiceItems: resource('/Sale/Invoice/Item', 'Items'),
   contactCustomers: resource('/Contact/Customer', 'Items'),
+  employees: resource('/Contact/Employee', 'Items'),
   taxCodes: resource('/GeneralLedger/TaxCode', 'Items'),
   accounts: resource('/GeneralLedger/Account', 'Items'),
+  jobs: resource('/GeneralLedger/Job', 'Items'),
+  categories: resource('/GeneralLedger/Category', 'Items'),
 };
